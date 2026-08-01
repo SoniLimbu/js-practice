f7a5ec6a090adc10703f5a7fa9046e356bf2d341
Soni Limbu <limbusoni297@gmail.com>
2026-08-01 23:19:36 +0545

    day2

---
+++ function.js
@@ -0,0 +1,22 @@
+///function without parameter
+function greet() {
+    console.log("hello Soni")
+}
+
+greet();
+
+///function with parameter
+function greetUser(name) {
+    console.log("hello " + name)
+}
+greetUser("Soni");
+greetUser("John");
+greetUser("Alice");
+
+///function with two parameters
+function add(a, b){
+    console.log(a + b);
+
+}
+add(5, 10);
+add(20, 30);
