89110a6be91fbd90a6903572f42416579d93a2e7
Soni Limbu <limbusoni297@gmail.com>
2026-08-05 21:52:52 +0545

    day 4  pratice

--- Nestedloop.js
+++ Nestedloop.js
@@ -5,3 +5,49 @@ for (let i =2; i<=2; i ++){
     }
 
 }
+
+///square pattern
+for (let i = 1; i <=4; i ++){
+    let row = "";
+    for (let j = 1; j <=4; j ++){
+        row += "* ";
+    }
+    console.log(row);
+}
+
+///number pattern
+for (let i = 1; i <=5; i ++){
+    let row = "";
+    for (let j = 1; j <=i; j ++){
+        row += j + " ";
+    }   
+    console.log(row);
+}
+
+/// Increasing Triangle 
+for (let i = 1; i <=5; i ++){
+    let row = "";   
+    for (let j = 1; j <=i; j ++){
+        row += "* ";
+    }           
+    console.log(row);
+}
+
+/// Decreasing Triangle
+for (let i = 5; i >=1; i --){
+    let row = "";       
+    for (let j = 1; j <=i; j ++){
+        row += "* ";
+    }   
+    console.log(row);
+}
+
+///Multiplication Table 
+for (let i = 1; i <=10; i ++){
+    let row = "";
+    for (let j = 1; j <=10; j ++){
+        row += (i * j) + " ";
+    }   
+    console.log(row);
+}
+    
